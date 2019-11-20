a0db598afd2e3ab50485b387391aabef30de1a09
roll1226 <tomo_yasu_4724@icloud.com>
2019-11-20 14:15:52 +0900

    change login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -1,38 +1,71 @@
 <template>
   <div>
-    <h1>
+    <h2>
       ログイン
-    </h1>
-    ユーザ名<input v-model="user" type="text" />
-    <br />
-    パスワード<input v-model="pass" type="password" />
-    <!--"v-vtn":ボタン-->
-    <v-btn color="red" @click="look">
-      みる
+    </h2>
+
+    <!--
+      rules -> もしuserに入ってなければエラーがでる
+     -->
+    <v-text-field
+      ref="name"
+      v-model="user"
+      :rules="[() => !!user || '入力しろ']"
+      label="ユーザ名"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="password"
+      :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+      :type="show1 ? 'text' : 'password'"
+      :rules="[rules.required, rules.min]"
+      name="input-10-1"
+      label="パスワード入力"
+      hint="8文字以上のパスワードを入力してください。"
+      counter
+      @click:append="show1 = !show1"
+    ></v-text-field>
+
+    <v-btn :loading="loading" color="promise" @click="login">
+      ログイン
     </v-btn>
-    <!--変数に入力値を格納-->
-    {{ user }}
-    <!--"v-show":trueの時見えて、falseの時見えないブロック-->
-    <div v-show="bool">
-      {{ pass }}
+    <v-btn v-show="loadingerror" :loading="loading" color="error">
+      ログイン失敗
+    </v-btn>
+
+    <div v-show="sigin">
+      ログイン成功
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-
+import { auth } from '@/plugins/firebase'
 @Component
-export default class Login extends Vue {
-  public user: string = '' // userの入力値string以外はダメ
-  public pass: string = ''
-  public bool: boolean = false
-
-  look() {
-    console.log('hoge1')
-    if (this.pass === '') return
-    console.log('hoge2')
-    this.bool = true
+export default class login extends Vue {
+  public user: string = ''
+  public password: string = ''
+  public show1: boolean = false
+  public loading: boolean = false
+  public loadingerror: boolean = false
+  public sigin: boolean = false
+  public rules: {} = {
+    required: (value: string) => !!value || 'Required.',
+    min: (v: string) => v.length >= 8 || 'Min 8 characters'
+  }
+  async login() {
+    this.loading = true
+    await auth
+      .signInWithEmailAndPassword(this.user, this.password)
+      .then(() => {
+        this.loading = false
+        this.sigin = true
+      })
+      .catch(() => {
+        this.loading = false
+        this.loadingerror = true
+      })
   }
 }
 </script>
