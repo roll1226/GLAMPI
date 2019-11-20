0f79b5e41834982f745942f9d4042326ca140457
roll1226 <tomo_yasu_4724@icloud.com>
2019-11-20 14:39:48 +0900

    add userAdmin.vue

--- pages/userAdmin.vue
+++ pages/userAdmin.vue
@@ -3,5 +3,17 @@
     <h1>
       会員登録
     </h1>
+    <v-text-field
+      v-model="name"
+      label="氏名"
+      placeholder="氏名を入力してね"
+    ></v-text-field>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class admin extends Vue {}
+</script>
