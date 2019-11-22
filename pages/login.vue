2ee224f2ec051da75c8e6a3e973e3c797a9b3c15
roll1226 <tomo_yasu_4724@icloud.com>
2019-11-23 00:44:09 +0900

    php decoy

--- pages/login.vue
+++ pages/login.vue
@@ -3,32 +3,26 @@
     <!--
       rules -> もしuserに入ってなければエラーがでる
      -->
-    <div v-if="!isLogin">
+    <div v-if="!test.data">
       <h2>
-        ログイン前
+        ログインしていません
       </h2>
       <LoginModal :user.sync="user" :password.sync="password" @login="login" />
-      {{ user }}
-      {{ password }}
     </div>
-
     <div v-else>
       <h2>
-        ログイン中
+        ログインしています
       </h2>
-      <v-btn @click="logout">
+      <v-btn color="red" @click="logout">
         ログアウト
       </v-btn>
     </div>
   </div>
 </template>
-
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import { auth } from '@/plugins/firebase'
-
+// import { auth } from '@/plugins/firebase'
 import LoginModal from '@/components/Btn/LoginModal.vue'
-
 @Component({
   components: {
     LoginModal
@@ -36,18 +30,19 @@ import LoginModal from '@/components/Btn/LoginModal.vue'
   computed: {
     isLogin(): boolean {
       return this.$store.state.login.isLogin
+    },
+    test(): boolean {
+      return this.$store.state.login.test
     }
   }
 })
 export default class login extends Vue {
   public user: string = ''
   public password: string = ''
-
   public rules: {} = {
     required: (value: string) => !!value || 'Required.',
     min: (v: string) => v.length >= 8 || 'Min 8 characters'
   }
-
   login() {
     this.$store.commit('login/SET_LOADING', true)
     this.$store.dispatch('login/loginEmailPass', {
@@ -55,11 +50,13 @@ export default class login extends Vue {
       password: this.password
     })
   }
-
-  async logout() {
-    await auth.signOut().then(() => {
-      this.$store.commit('login/IS_LOGIN', false)
-    })
+  logout() {
+    // await auth.signOut().then(() => {
+    //   this.$store.commit('login/IS_LOGIN', false)
+    //   this.password = ''
+    //   this.user = ''
+    // })
+    this.$store.dispatch('login/loginOut', false)
   }
 }
 </script>
--- store/login.ts
+++ store/login.ts
@@ -1,43 +1,67 @@
 import * as Vuex from 'vuex'
+// import * as axios from 'axios'
+const { default: axios } = require('axios')
 import { auth } from '@/plugins/firebase'
-
 interface ICommit {
   commit: Vuex.Commit
 }
-
 interface IState {
   loading: boolean
   isLogin: boolean
+  test: object[]
 }
-
 export const state = (): IState => ({
   loading: false,
-  isLogin: false
+  isLogin: false,
+  test: []
 })
-
 export const mutations = {
   SET_LOADING(state: IState, payload: boolean) {
     state.loading = payload
   },
-
   IS_LOGIN(state: IState, payload: boolean) {
     state.isLogin = payload
+  },
+  SET_TEST(state: IState, payload: object[]) {
+    state.test = payload
   }
 }
-
 export const actions = {
   async loginEmailPass(
     dispatch: ICommit,
     payload: { user: string; password: string }
   ) {
-    await auth
-      .signInWithEmailAndPassword(payload.user, payload.password)
-      .then(() => {
+    // await auth
+    //   .signInWithEmailAndPassword(payload.user, payload.password)
+    //   .then(() => {
+    //     dispatch.commit('SET_LOADING', false)
+    //     dispatch.commit('IS_LOGIN', true)
+    //   })
+    //   .catch(() => {
+    //     dispatch.commit('SET_LOADING', false)
+    //   })
+    const params = new URLSearchParams()
+    params.append('user', payload.user)
+    params.append('passwd', payload.password)
+    params.append('login', 'aaa')
+    await axios
+      .post('http://localhost:8080/GLAMPY/login.php', params)
+      .then((response: object[]) => {
+        dispatch.commit('SET_TEST', response)
         dispatch.commit('SET_LOADING', false)
-        dispatch.commit('IS_LOGIN', true)
+        // dispatch.commit('IS_LOGIN', true)
       })
-      .catch(() => {
+  },
+
+  async loginOut(dispatch: ICommit, payload: boolean) {
+    const params = new URLSearchParams()
+    params.append('logout', 'aaa')
+    await axios
+      .post('http://localhost:8080/GLAMPY/login.php', params)
+      .then((response: object[]) => {
+        dispatch.commit('SET_TEST', response)
         dispatch.commit('SET_LOADING', false)
+        // dispatch.commit('IS_LOGIN', true)
       })
   }
 }
