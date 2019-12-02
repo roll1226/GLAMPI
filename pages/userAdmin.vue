e4202d5a728fb86627ff2889be64f273380f45b7
osaki <kookm1@mkmnomacbookpro.local>
2019-12-02 15:59:33 +0900

    [update]

--- pages/userAdmin.vue
+++ pages/userAdmin.vue
@@ -1,19 +1,110 @@
 <template>
   <div>
-    <h1>
-      会員登録
-    </h1>
-    <v-text-field
-      v-model="name"
-      label="氏名"
-      placeholder="氏名を入力してね"
-    ></v-text-field>
+    <!-- <h1>会員登録</h1> -->
+    <!-- /*氏名*/
+    /*漢字*/-->
+    <!-- <v-text-field v-model="sei" label="姓" placeholder="姓"></v-text-field>
+    <v-text-field v-model="mei" label="名" placeholder="名"></v-text-field> -->
+    <!-- /*ひらがな*/ -->
+    <!-- <v-text-field v-model="sei" label="姓" placeholder="セイ"></v-text-field>
+    <v-text-field v-model="mei" label="名" placeholder="メイ"></v-text-field>
+    <input type="radio" id="M" value="M" v-model="picked" />
+    <label for="M">男性</label>
+    <br />
+    <input type="radio" id="F" value="F" v-model="picked" />
+    <label for="F">女性</label>
+    <br /> -->
+    <!-- //生年月日 -->
+    <v-menu
+      ref="menu"
+      v-model="menu"
+      :close-on-content-click="false"
+      transition="scale-transition"
+      offset-y
+      full-width
+      min-width="290px"
+    >
+      <template v-slot:activator="{ on }">
+        <v-text-field
+          v-model="date"
+          label="生年月日"
+          readonly
+          v-on="on"
+        ></v-text-field>
+      </template>
+      <v-date-picker
+        ref="picker"
+        v-model="date"
+        :max="new Date().toISOString().substr(0, 10)"
+        min="1950-01-01"
+        @change="save"
+      ></v-date-picker>
+    </v-menu>
+    <!--郵便番号-->
+    <v-card>
+      <v-card-text>
+        <v-text-field
+          v-model="value"
+          v-mask="address"
+          label="郵便番号番号入力"
+        ></v-text-field>
+      </v-card-text>
+    </v-card>
+    <v-text-field v-model="sei" label="住所" placeholder="住所"></v-text-field>
+
+    <!--出力-->
+    <!--<span>Picked: {{ picked }}</span>-->
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, Vue, Watch } from 'nuxt-property-decorator'
+// 郵便番号
+const { mask } = require('vue-the-mask')
+// API
+import axios from 'axios'
 
-@Component
-export default class admin extends Vue {}
+// 生年月日
+// export default {
+//   data: () => ({
+//     date: null,
+//     menu: false
+//   }),
+//   watch: {
+//     menu(val) {
+//       val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
+//     }
+//   },
+//   methods: {
+//     save(date) {
+//       this.$refs.menu.save(date)
+//     }
+//   }
+// }
+
+@Component({
+  // 登録
+  directives: {
+    mask
+  },
+  data() {
+    return {
+      address: '###-####'
+    }
+  }
+})
+export default class admin extends Vue {
+  public date: string = ''
+  public menu: boolean = false
+  public picked: string = ''
+  @Watch('menu')
+  seMenu(val: string) {
+    val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
+  }
+
+  save(date: string) {
+    this.$refs.menu.save(date)
+    console.log(date)
+  }
+}
 </script>
