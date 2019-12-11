aa541eec2fff9cfdadb45c304190b442f3e762d9
roll1226 <tomo_yasu_4724@icloud.com>
2019-12-11 23:22:32 +0900

    [ADD]introduction.vue[UPDATE]position/reservation

---
+++ pages/facility/_id/introduction.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>
+      施設紹介
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class template extends Vue {}
+</script>
