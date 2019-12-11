88d4b6bd94d2d63d0fb96d8a8265028901168714
iwaya masataka <glosbe_kh@softbank.ne.jp>
2019-12-11 14:13:58 +0900

    [UPDATE]userRegistration-conponent

---
+++ components/UserRegistration/password.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <v-text-field
+      v-model="password"
+      :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+      :type="show1 ? 'text' : 'password'"
+      name="input-10-1"
+      label="パスワード"
+      prepend-icon="mdi-lock"
+      :rules="[rules.isPassword, rules.passwordLength, rules.passwordFormat]"
+      counter
+      @click:append="show1 = !show1"
+    ></v-text-field>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class passwordUserRegistration extends Vue {
+  public password: string = ''
+  public rules: {} = {
+    isPassword: (v: string) => !!v || 'パスワードは必ず入力してください。',
+    passwordLength: (v: string) =>
+      (v.length <= 20 && v.length >= 6) ||
+      'パスワードは6桁以上20桁以内にて入力してください。',
+    passwordFormat: (v: string) => {
+      const pattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/
+      return (
+        pattern.test(v) ||
+        'パスワードは半角英字と、数字の両方を必ず入れてください。'
+      )
+    }
+  }
+}
+</script>
---
+++ components/UserRegistration/username.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-text-field
+      v-model="username"
+      label="ユーザ名"
+      prepend-icon="mdi-account-circle"
+      :rules="rules.usernameLength"
+    ></v-text-field>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class passwordUserRegistration extends Vue {
+  public username: string = ''
+  public rules: {} = {
+    usernameLength: (v: string) =>
+      (v && v.length <= 20) || 'ユーザ名は20字以内にて入力してください。'
+  }
+}
+</script>
