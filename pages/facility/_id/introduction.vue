737f3d4e289237e2e40135ee746f71ed5c3a60ec
roll1226 <tomo_yasu_4724@icloud.com>
2019-12-12 11:58:05 +0900

    [UPDATE]v-carousel for introduction.vue

--- pages/facility/_id/introduction.vue
+++ pages/facility/_id/introduction.vue
@@ -3,12 +3,44 @@
     <h1>
       施設紹介
     </h1>
+
+    <v-carousel
+      cycle
+      hide-delimiter-background
+      show-arrows-on-hover
+      width="500"
+    >
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 
+interface IItem {
+  src: string
+}
+
 @Component
-export default class template extends Vue {}
+export default class template extends Vue {
+  items: IItem[] = [
+    {
+      src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+    },
+    {
+      src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+    },
+    {
+      src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+    },
+    {
+      src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+    }
+  ]
+}
 </script>
