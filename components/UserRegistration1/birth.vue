7989694ba3d01029505d756999794c843d058163
osaki <kookm1@mkmnomacbookpro.local>
2019-12-27 10:55:52 +0900

    [fix]userregistration

---
+++ components/UserRegistration1/birth.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <script lang="ts">
+      <v-row>
+      <v-col cols="12" sm="4">
+        <v-overflow-btn :items="dropdown"></v-overflow-btn>
+      </v-col>
+    </v-row>
+    </script>
+  </div>
+</template>
