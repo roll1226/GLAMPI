f9e6cfc975fcb3512c2370144beb83d8efa098d2
iwaya masataka <glosbe_kh@softbank.ne.jp>
2019-12-29 00:12:40 +0900

    [FIX]userRagistration

--- components/UserRegistration/password.vue
+++ components/UserRegistration/password.vue
@@ -4,7 +4,6 @@
       v-model="password"
       :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
       :type="show1 ? 'text' : 'password'"
-      name="input-10-1"
       label="パスワード"
       prepend-icon="mdi-lock"
       :rules="[rules.isPassword, rules.passwordLength, rules.passwordFormat]"
@@ -16,9 +15,9 @@
       :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
       :type="show2 ? 'text' : 'password'"
       label="パスワード確認"
-      prepend-icon="mdi-lock"
+      prepend-icon="mdi-"
       hint="パスワードをもう一度入力してください。"
-      :rules="[rules.passwordLegend]"
+      :rules="[rules.isPasswordCheck, rules.passwordCheck]"
       counter
       @click:append="show2 = !show2"
     ></v-text-field>
@@ -45,7 +44,9 @@ export default class passwordUserRegistration extends Vue {
         'パスワードは半角英字と、数字の両方を必ず入れてください。'
       )
     },
-    passwordLegend: (v: string) =>
+    isPasswordCheck: (v: string) =>
+      !!v || 'パスワード確認は必ず入力してください',
+    passwordCheck: (v: string) =>
       v === this.password || 'パスワードが一致していません。'
   }
 }
