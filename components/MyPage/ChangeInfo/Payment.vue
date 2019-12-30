68f946e9c11ba21e13d63476a265bd468f0e86cb
iwaya masataka <glosbe_kh@softbank.ne.jp>
2019-12-30 18:44:04 +0900

    [UPDATE]Payment.vue

--- components/MyPage/ChangeInfo/Payment.vue
+++ components/MyPage/ChangeInfo/Payment.vue
@@ -1,19 +1,48 @@
 <template>
   <v-card width="850">
     <v-card-title>支払い方法変更</v-card-title>
-    <!--基本情報変更ページは入力欄に初期値で現在の情報を表示する。-->
-    <!--コンポーネントは登録の時のだけどどうするのか？-->
     <v-form>
       <v-container>
         <v-row>
           <v-col>
             <v-text-field
-              v-model="paymentID"
+              v-model="paymentCardID"
               label="カード番号"
               :rules="[
-                rules.isPaymentID,
-                rules.paymentIDLength,
-                rules.paymentIDFormat
+                rules.isPaymentCardID,
+                rules.paymentCardIDLength,
+                rules.paymentCardIDFormat
+              ]"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" sm="4">
+            <v-overflow-btn
+              v-model="paymentCardMonth"
+              :items="dropdown"
+              label="有効期限：月"
+              :rules="[rules.isPaymentCardMonth]"
+            ></v-overflow-btn>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <v-overflow-btn
+              v-model="paymentCardYear"
+              :items="dropdown1"
+              label="有効期限：年"
+              :rules="[rules.isPaymentCardYear]"
+            ></v-overflow-btn>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col sm="3">
+            <v-text-field
+              v-model="paymentCardSecurityNumber"
+              label="セキュリティ番号"
+              :rules="[
+                rules.isPaymentCardSecurityNumber,
+                rules.paymentCardSecurityNumberLength,
+                rules.paymentCardSecurityNumberFormat
               ]"
             ></v-text-field>
           </v-col>
@@ -28,17 +57,46 @@ import { Component, Vue } from 'nuxt-property-decorator'
 
 @Component
 export default class Payment extends Vue {
-  public paymentID: string = ''
-  // public paymentID: string = ''
-  // public paymentID: string = ''
+  public paymentCardID: string = ''
+  public paymentCardMonth: string = ''
+  public paymentCardYear: string = ''
+  public paymentCardSecurityNumber: string = ''
+  dropdown: [...string[]] = []
+  dropdown1: [...string[]] = []
 
   public rules: {} = {
-    isPaymentID: (v: string) => !!v || 'カード番号は必ず入力してください。',
-    paymentIDLength: (v: string) =>
-      (v && v.length === 16) || 'カード番号は16桁で入力してください',
-    paymentIDFormat: (v: string) => {
+    isPaymentCardID: (v: string) => !!v || 'カード番号は必ず入力してください。',
+    paymentCardIDLength: (v: string) =>
+      (v && v.length === 16) || 'カード番号は16桁にて入力してください',
+    paymentCardIDFormat: (v: string) => {
       const pattern = /^\d{16}$/
-      return pattern.test(v) || 'カード番号は半角数字で入力してください。'
+      return pattern.test(v) || 'カード番号は半角数字にて入力してください。'
+    },
+    isPaymentCardMonth: (v: string) =>
+      !!v || '有効期限：月は必ず入力してください。',
+    isPaymentCardYear: (v: string) =>
+      !!v || '有効期限：年は必ず入力してください。',
+    isPaymentCardSecuritynumber: (v: string) =>
+      !!v || 'セキュリティ番号は必ず入力してください。',
+    paymentCardSecurityNumberLength: (v: string) =>
+      (v && v.length === 3) || 'セキュリティ番号は3桁にて入力してください',
+    paymentCardSecurityNumberFormat: (v: string) => {
+      const pattern = /^\d{3}$/
+      return (
+        pattern.test(v) || 'セキュリティ番号は半角数字にて入力してください。'
+      )
+    }
+  }
+
+  created() {
+    for (let index1 = 0; index1 < 12; index1++) {
+      const month = 1
+      this.dropdown.push(String(month + index1))
+
+      for (let index = 0; index < 101; index++) {
+        const year = 2020
+        this.dropdown1.push(String(year + index))
+      }
     }
   }
 }
