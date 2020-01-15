b40a7a2d2098fbc7d8e2b53dccabb43df9274978
iwaya masataka <glosbe_kh@softbank.ne.jp>
2020-01-15 14:59:15 +0900

    [ADD]createGlammity.vue

---
+++ pages/Glammity/createGlammity.vue
@@ -0,0 +1,66 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({
+  components: {
+    GlammityName,
+    facilityName,
+    plan,
+    recruitmentNumbers,
+    schedule
+  }
+})
+export default class template extends Vue {
+  get a(): string {
+    return this.$store.state.registration.GlammityName
+  }
+
+  set a(value: string) {
+    this.$store.commit('createGlammity/SET_GLAMMITY_NAME', value)
+  }
+
+  get b(): string {
+    return this.$store.state.registration.facilityName
+  }
+
+  set b(value: string) {
+    this.$store.commit('createGlammity/SET_FACILITY_NAME', value)
+  }
+
+  get c(): string {
+    return this.$store.state.registration.plan
+  }
+
+  set c(value: string) {
+    this.$store.commit('createGlammity/SET_PLAN', value)
+  }
+
+  get d(): string {
+    return this.$store.state.registration.recruitmentNumbers
+  }
+
+  set d(value: string) {
+    this.$store.commit('createGlammity/SET_RECRUITMENT_NUMBERS', value)
+  }
+
+  get e(): string {
+    return this.$store.state.registration.chedule
+  }
+
+  set e(value: string) {
+    this.$store.commit('createGlammity/SET_SCHEDULE', value)
+  }
+
+  public rules: {} = {
+    a1: (v:string)
+    a2: (v: string) => {
+      const pattern = /^[亜-黑]{1,20}$/
+      return pattern.test(v) || '姓は必ず漢字で20文字以内にて入力してください。'
+    }
+  }
+}
+</script>
---
+++ store/createGlammity.ts
@@ -0,0 +1,43 @@
+import * as Vuex from 'vuex'
+
+interface ICommit {
+  commit: Vuex.Commit
+}
+
+interface IState {
+  GlammityName: string
+  facilityName: string
+  plan: string
+  recruitmentNumbers: string
+  schedule: string
+}
+
+export const state = (): IState => ({
+  GlammityName: '',
+  facilityName: '',
+  plan: '',
+  recruitmentNumbers: '',
+  schedule: '',
+})
+
+export const mutations = {
+  SET_GLAMMITY_NAME(state: IState, payload: string) {
+    state.GlammityName = payload
+  },
+
+  SET_FACILITY_NAME(state: IState, payload: string) {
+    state.facilityName = payload
+  },
+
+  SET_PLAN(state: IState, payload: string) {
+    state.plan = payload
+  },
+
+  SET_RECRUITMENT_NUMBERS(state: IState, payload: string) {
+    state.recruitmentNumbers = payload
+  },
+
+  SET_SCHEDULE(state: IState, payload: string) {
+    state.schedule = payload
+  }
+}
