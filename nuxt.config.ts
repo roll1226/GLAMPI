64c482a2dd4ace298614fc5c0465bd9ea1fa73a2
roll1226 <tomo_yasu_4724@icloud.com>
2020-01-24 21:09:03 +0900

    [UPDATE]add index-tag-mobile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default {
   /*
    ** Headers of the page
    */
+  server: {
+    host: '0.0.0.0'
+  },
   head: {
     title: process.env.npm_package_name || '',
     script: [{ src: 'https://js.stripe.com/v3/' }],
