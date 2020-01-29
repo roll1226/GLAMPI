6362a3dac658c33c8a9d332197a40d2aaaddd3ae
iwaya masataka <glosbe_kh@softbank.ne.jp>
2020-01-29 12:44:08 +0900

    [ADD]facilityRegistration&components

---
+++ components/FacilityRegistration/facilityName.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-text-field
+      v-model="resPerson"
+      label="施設名"
+      prepend-icon="mdi-"
+      :rules="[rules.isFacilityName]"
+    ></v-text-field>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class facilityNameFacilityRegistration extends Vue {
+  // public sei: string = ''
+  get sei(): string {
+    return this.$store.state.registration.firstName
+  }
+
+  set sei(value: string) {
+    this.$store.commit('registration/SET_FIRST_NAME', value)
+  }
+  public rules: {} = {
+    isFacilityName: (v: string) =>
+      (v.length >= 1 && v.length <= 20) ||
+      !!v ||
+      '施設名は必ず入力してください。'
+  }
+}
+</script>
