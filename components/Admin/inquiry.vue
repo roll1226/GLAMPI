5e54c4f13b9a3d2b4dc71a78076a07f0e474e126
iwaya masataka <glosbe_kh@softbank.ne.jp>
2020-02-05 20:26:15 +0900

    [FIX]Admin-components

--- components/Admin/inquiry.vue
+++ components/Admin/inquiry.vue
@@ -1,16 +1,48 @@
 <template>
   <div>
     <v-row class="ma-0 pa-0">
+      <v-col cols="auto" class="ma-0 pa-0">
+        <v-tabs
+          class="pa-0 ma-0 tabs"
+          background-color="rgb(244,244,244)"
+          active-class="font-weight-bold grey lighten-2"
+          hide-slider
+        >
+          <v-tab class="black--text" elevation="0" @click="tab = 0"
+            >未送信</v-tab
+          >
+          <v-tab class="black--text" elevation="0" @click="tab = 1"
+            >送信済</v-tab
+          >
+        </v-tabs>
+      </v-col>
+    </v-row>
+    <v-row class="ma-0 pa-0">
       <v-col class="ma-0 pa-0">
-        <v-data-table
-          :headers="headers"
-          :items="list"
-          :page.sync="page"
-          height="500px"
-          hide-default-footer
-          class="ma-0 pa-0"
-          @page-count="pageCount = $event"
-        ></v-data-table>
+        <v-tabs-items class="item">
+          <div v-if="tab == 0">
+            <v-data-table
+              class="ma-0 pa-0"
+              height="521.875px"
+              :headers="headers"
+              :items="list"
+              :page.sync="page"
+              hide-default-footer
+              @page-count="pageCount = $event"
+            ></v-data-table>
+          </div>
+          <div v-else-if="tab == 1">
+            <v-data-table
+              class="ma-0 pa-0"
+              height="521.875px"
+              :headers="headers2"
+              :items="list2"
+              :page.sync="page"
+              hide-default-footer
+              @page-count="pageCount = $event"
+            ></v-data-table>
+          </div>
+        </v-tabs-items>
       </v-col>
     </v-row>
     <v-row class="text-center pt-2">
@@ -39,6 +71,7 @@ import { Component, Vue } from 'nuxt-property-decorator'
       page: 1,
       pageCount: 0,
       itemsPerPage: 10,
+      isColor: 'grey lighten-2',
       headers: [
         { text: 'ID', value: 'id' },
         { text: '人数', value: 'peopleNumber' },
@@ -47,32 +80,66 @@ import { Component, Vue } from 'nuxt-property-decorator'
       list: [
         {
           id: '1',
-          peopleNumber: '8',
+          peopleNumber: '3',
+          plan: 'プランA'
+        },
+        {
+          id: '2',
+          peopleNumber: '6',
+          plan: 'プランB'
+        },
+        {
+          id: '3',
+          peopleNumber: '9',
           plan: 'プランC'
         },
         {
-          id: '1',
-          peopleNumber: '8',
-          plan: 'プランC'
+          id: '4',
+          peopleNumber: '12',
+          plan: 'プランD'
         },
         {
-          id: '1',
-          peopleNumber: '8',
-          plan: 'プランC'
+          id: '5',
+          peopleNumber: '15',
+          plan: 'プランE'
+        }
+      ],
+      headers2: [
+        { text: 'ID', value: 'id' },
+        { text: '人数', value: 'peopleNumber' },
+        { text: 'プラン', value: 'plan' }
+      ],
+      list2: [
+        {
+          id: '6',
+          peopleNumber: '4',
+          plan: 'プランXX'
         },
         {
-          id: '1',
+          id: '7',
           peopleNumber: '8',
-          plan: 'プランC'
+          plan: 'プランXP'
         },
         {
-          id: '1',
-          peopleNumber: '8',
-          plan: 'プランC'
+          id: '8',
+          peopleNumber: '12',
+          plan: 'プランDX'
+        },
+        {
+          id: '9',
+          peopleNumber: '16',
+          plan: 'プランSX'
+        },
+        {
+          id: '10',
+          peopleNumber: '20',
+          plan: 'プランCV'
         }
       ]
     }
   }
 })
-export default class FacilityIndex extends Vue {}
+export default class FacilityIndex extends Vue {
+  public tab: number = 0
+}
 </script>
