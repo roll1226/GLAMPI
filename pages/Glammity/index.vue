4c8191079edcf0ac6b8ded16b76d87385d6a7eaf
roll1226 <tomo_yasu_4724@icloud.com>
2020-02-06 20:37:45 +0900

    [ADD]glammity index

---
+++ pages/Glammity/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <v-row>
+      <v-col lg="3" md="3" sm="3" xs="12">
+        <v-row>
+          <v-col>
+            <v-card>
+              <v-card-title>
+                aaaaa
+              </v-card-title>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col>
+        <v-row>
+          <v-col
+            v-for="(inte, index) in 10"
+            :key="index"
+            lg="6"
+            md="6"
+            sm="6"
+            xs="12"
+          >
+            <v-card>
+              <div class="d-flex flex-no-wrap justify-start">
+                <v-avatar class="ma-3" size="125" tile>
+                  <v-img
+                    src="https://pbs.twimg.com/profile_images/1063562329155989505/f3HAhG-8.jpg"
+                  ></v-img>
+                </v-avatar>
+
+                <div>
+                  <v-card-title
+                    class="headline"
+                    v-text="'タイトル'"
+                  ></v-card-title>
+
+                  <v-card-subtitle v-text="'サブタイトル'"></v-card-subtitle>
+                </div>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </div>
+</template>
