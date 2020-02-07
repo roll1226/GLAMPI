37d56e37a55a00bab725c3fae35c2702cc9e5fa6
roll1226 <tomo_yasu_4724@icloud.com>
2020-02-07 10:56:47 +0900

    [UPDATE]Glammpy index,vue

--- pages/Glammity/index.vue
+++ pages/Glammity/index.vue
@@ -6,7 +6,7 @@
           <v-col>
             <v-card>
               <v-card-title>
-                aaaaa
+                glammity検索
               </v-card-title>
             </v-card>
           </v-col>
@@ -29,14 +29,39 @@
                     src="https://pbs.twimg.com/profile_images/1063562329155989505/f3HAhG-8.jpg"
                   ></v-img>
                 </v-avatar>
-
                 <div>
-                  <v-card-title
-                    class="headline"
-                    v-text="'タイトル'"
-                  ></v-card-title>
+                  <v-card-text>
+                    <p>
+                      施設名: hogehoge
+                    </p>
 
-                  <v-card-subtitle v-text="'サブタイトル'"></v-card-subtitle>
+                    <div>
+                      主催者: 俺
+                    </div>
+                    <div>
+                      募集期間: 2020/01/23~2020/02/10
+                    </div>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-row>
+                      <v-col cols="6" class="pb-0">
+                        <v-btn block text>
+                          施設紹介
+                        </v-btn>
+                      </v-col>
+                      <v-col cols="6" class="pb-0">
+                        <v-btn block text>
+                          紹介ページ
+                        </v-btn>
+                      </v-col>
+                      <v-col cols="12" class="pb-1 pt-2">
+                        <v-btn block text>
+                          参加する
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-card-actions>
                 </div>
               </div>
             </v-card>
