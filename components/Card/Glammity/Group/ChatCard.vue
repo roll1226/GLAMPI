124e45382b258062fbb8daf7bcb7ddeb315398e1
roll1226 <tomo_yasu_4724@icloud.com>
2020-02-10 23:11:51 +0900

    [UPDATE]connection firestore

--- components/Card/Glammity/Group/ChatCard.vue
+++ components/Card/Glammity/Group/ChatCard.vue
@@ -5,14 +5,49 @@
     </v-card-title>
 
     <v-card-text>
-      チャット
+      <v-card
+        v-for="(message, index) in glammityMessage"
+        :key="index"
+        class="mt-1"
+        outlined
+      >
+        <v-card-actions class="pb-0">
+          <v-list-item class="grow">
+            <v-list-item-avatar
+              max-width="50"
+              width="50"
+              height="50"
+              color="grey darken-3"
+            >
+              <v-img class="elevation-1" :src="message.userImg"></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title v-text="message.userName"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card-actions>
+
+        <v-card-text class="ml-3 body-1" v-text="message.message"></v-card-text>
+      </v-card>
     </v-card-text>
   </v-card>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
+import { IMessage } from '@/store/glammityGroup'
 
 @Component
-export default class ChatCard extends Vue {}
+export default class ChatCard extends Vue {
+  created() {
+    this.$store.commit('glammityGroup/RESET_MESSAGE')
+    const url = this.$route.params.Glammity
+    this.$store.dispatch('glammityGroup/getMessage', url)
+  }
+
+  get glammityMessage(): IMessage[] {
+    return this.$store.state.glammityGroup.message
+  }
+}
 </script>
--- store/glammityGroup.ts
+++ store/glammityGroup.ts
@@ -1,20 +1,29 @@
 import * as Vuex from 'vuex'
-// import { firestore } from '@/plugins/firebase'
+import { firestore } from '@/plugins/firebase'
 
 export type userStates = 'host' | 'guest'
 
 interface ICommit {
   commit: Vuex.Commit
+  dispatch: Vuex.Dispatch
+}
+
+export interface IMessage {
+  userName: string
+  userImg: string
+  message: string
 }
 
 interface IState {
   tabNumber: number
   userStates: userStates
+  message: IMessage[]
 }
 
 export const state = (): IState => ({
   tabNumber: 0,
-  userStates: 'host'
+  userStates: 'host',
+  message: []
 })
 
 export const mutations = {
@@ -24,5 +33,63 @@ export const mutations = {
 
   SET_USER_STATES(state: IState, payload: userStates) {
     state.userStates = payload
+  },
+
+  SET_MESSAGE(
+    state: IState,
+    payload: {
+      user: {
+        userImg: string
+        firstName: string
+        lastName: string
+        nickname: string
+      }
+      message: string
+    }
+  ) {
+    const message = {
+      userName:
+        payload.user.nickname !== ''
+          ? payload.user.nickname
+          : payload.user.lastName + payload.user.firstName,
+      userImg: payload.user.userImg,
+      message: payload.message
+    }
+    state.message.push(message)
+  },
+
+  RESET_MESSAGE(state: IState) {
+    state.message = []
+  }
+}
+
+export const actions = {
+  async getMessage(dispatch: ICommit, payload: string) {
+    const messages = await firestore
+      .collection('glammity')
+      .doc(payload)
+      .collection('messages')
+      .get()
+
+    // const items = message.docs.map((doc) => doc.data())
+    // console.log(items)
+    for (const message of messages.docs) {
+      dispatch.dispatch('getUser', message.data())
+    }
+  },
+
+  async getUser(
+    dispatch: ICommit,
+    payload: { message: string; userId: string }
+  ) {
+    const user = await firestore
+      .collection('users')
+      .doc(payload.userId)
+      .get()
+
+    dispatch.commit('SET_MESSAGE', {
+      user: user.data(),
+      message: payload.message
+    })
   }
 }
