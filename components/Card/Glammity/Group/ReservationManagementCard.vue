eb874697bdd7509d48b86984af4266e38809ec3e
roll1226 <tomo_yasu_4724@icloud.com>
2020-02-27 21:01:48 +0900

    [UPDATE]host functions

--- components/Card/Glammity/Group/ReservationManagementCard.vue
+++ components/Card/Glammity/Group/ReservationManagementCard.vue
@@ -3,12 +3,61 @@
     <v-card-text>
       予約管理
     </v-card-text>
+
+    <v-row>
+      <GlammityInfoCard :title="glammityName.title" :info="glammityName.info" />
+
+      <GlammityInfoCard :title="facilityName.title" :info="facilityName.info" />
+
+      <GlammityInfoCard :title="plan.title" :info="plan.info" />
+
+      <GlammityInfoCard
+        :title="estimatedAmount.title"
+        :info="estimatedAmount.info"
+      />
+    </v-row>
   </v-card>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
+import GlammityInfoCard from '@/components/Card/Glammity/Group/GlammityInfoCard.vue'
+import { IGlammityInfoList } from '@/store/glammityInfo'
 
-@Component
-export default class ReservationManagementCard extends Vue {}
+@Component({
+  components: {
+    GlammityInfoCard
+  }
+})
+export default class ReservationManagementCard extends Vue {
+  created() {
+    this.$store.commit('glammityInfo/CLEAR_INFO')
+    const url = this.$route.params.Glammity
+    this.$store.dispatch('glammityInfo/getGlammityInfo', url)
+  }
+
+  get glammityName(): IGlammityInfoList {
+    return this.$store.state.glammityInfo.glammityName
+  }
+
+  get facilityName(): IGlammityInfoList {
+    return this.$store.state.glammityInfo.facilityName
+  }
+
+  get plan(): IGlammityInfoList {
+    return this.$store.state.glammityInfo.plan
+  }
+
+  get numberOfApplicants(): IGlammityInfoList {
+    return this.$store.state.glammityInfo.numberOfApplicants
+  }
+
+  get numberOfParticipants(): IGlammityInfoList {
+    return this.$store.state.glammityInfo.numberOfParticipants
+  }
+
+  get estimatedAmount(): IGlammityInfoList {
+    return this.$store.state.glammityInfo.estimatedAmount
+  }
+}
 </script>
