2c0308624934dec39bd5603e26ffb24c3ced5b7c
roll1226 <tomo_yasu_4724@icloud.com>
2020-03-07 17:29:04 +0900

    [UPDATE]Chart to typescript

---
+++ components/Admin/GuestsNumberChard.vue
@@ -0,0 +1,93 @@
+<script lang="ts">
+import { mixins, Component, Prop } from 'nuxt-property-decorator'
+import Chart from 'chart.js'
+import { Bar } from 'vue-chartjs'
+import moment from 'moment'
+
+import { firestore } from '@/plugins/firebase'
+
+const month = [
+  '1月',
+  '2月',
+  '3月',
+  '4月',
+  '5月',
+  '6月',
+  '7月',
+  '8月',
+  '9月',
+  '10月',
+  '11月',
+  '12月'
+]
+
+@Component
+export default class GuestsNumberchard extends mixins(Bar) {
+  chartData: Chart.ChartData = {
+    labels: month,
+    datasets: [
+      {
+        label: '宿泊人数',
+        data: [],
+        borderColor: '#4444ff',
+        fill: false,
+        type: 'line',
+        lineTension: 0
+      }
+    ]
+  }
+
+  @Prop({ required: true })
+  chartOptions!: Chart.ChartOptions
+
+  async mounted() {
+    this.renderChart(this.chartData, this.chartOptions)
+
+    const facilities = firestore.collection('facilities')
+    const year = moment(new Date()).format('YYYY')
+    const month = Number(moment(new Date()).format('M'))
+
+    const facility = await facilities
+      .where('displayName', '==', this.$route.params.id)
+      .get()
+
+    console.log(facility)
+
+    const getMonth = [
+      'january',
+      'february',
+      'march',
+      'april',
+      'may',
+      'june',
+      'july',
+      'august',
+      'september',
+      'october',
+      'november',
+      'december'
+    ]
+
+    for (let index = 0; index < month; index++) {
+      const profitData = await facilities
+        .doc(facility.docs[0].id)
+        .collection('profit')
+        .doc(year)
+        .collection(getMonth[index])
+        .get()
+      console.log(profitData.docs[0].data().guests)
+
+      if (this.chartData.datasets) {
+        if (this.chartData.datasets[0].data) {
+          await this.chartData.datasets[0].data.push(
+            profitData.docs[0].data().guests
+          )
+        }
+      }
+    }
+
+    this.renderChart(this.chartData, this.chartOptions)
+  }
+}
+</script>
+<style></style>
--- components/Admin/revenue.vue
+++ components/Admin/revenue.vue
@@ -7,18 +7,24 @@
             <v-col class="text-center pa-0">
               <p class="ma-0 title">
                 今月<br /><span class="revenue"
-                  >収益:{{ revenue.toLocaleString() }}円</span
-                ><br /><span class="revenue">宿泊人数:{{ guests }}人</span
-                ><br /><span class="revenue">仲介料:{{ brokerageFee }}円</span>
+                  >収益:{{ berforIncomeNumber.toLocaleString() }}円</span
+                ><br /><span class="revenue"
+                  >宿泊人数:{{ berforGuestsNumber }}人</span
+                ><br /><span class="revenue"
+                  >仲介料:{{ berforBrokerageFee.toLocaleString() }}円</span
+                >
               </p></v-col
             >
 
             <v-col class="text-center pa-0">
               <p class="ma-0 title">
                 先月<br /><span class="revenue"
-                  >収益:{{ revenue2.toLocaleString() }}円</span
-                ><br /><span class="revenue">宿泊人数:{{ guests2 }}人</span
-                ><br /><span class="revenue">仲介料:{{ brokerageFee2 }}円</span>
+                  >収益:{{ toIncomeNumber.toLocaleString() }}円</span
+                ><br /><span class="revenue"
+                  >宿泊人数:{{ toGuestsNumber }}人</span
+                ><br /><span class="revenue"
+                  >仲介料:{{ toBrokerageFee.toLocaleString() }}円</span
+                >
               </p>
             </v-col>
           </v-row>
@@ -26,10 +32,18 @@
       </v-card>
       <v-row justify="center">
         <v-col cols="auto">
-          <Chart :height="500" :width="1000"></Chart>
+          <IncomeChart
+            :chart-options="incomeChartOption"
+            :height="500"
+            :width="1000"
+          />
         </v-col>
         <v-col cols="auto">
-          <Chart2 :height="500" :width="1000"></Chart2>
+          <GuestsNumberChard
+            :chart-options="guestsNumberChartOption"
+            :height="500"
+            :width="1000"
+          />
         </v-col>
       </v-row>
     </v-row>
@@ -38,24 +52,88 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import Chart from '@/components/Admin/Chart.vue'
-import Chart2 from '@/components/Admin/Chart_.vue'
+import Chart from 'chart.js'
+import IncomeChart from '@/components/Admin/IncomeChart.vue'
+import GuestsNumberChard from '@/components/Admin/GuestsNumberChard.vue'
 
 @Component({
   components: {
-    Chart,
-    Chart2
+    IncomeChart,
+    GuestsNumberChard
   }
 })
 export default class RevenueFacility extends Vue {
-  public revenue: number = 240000
-  public guests: number = 32
-  public revenue2: number = 520000
-  public guests2: number = 67
-  public brokerageFee: number = this.revenue / 10
-  public brokerageFee2: number = this.revenue2 / 10
+  berforIncomeNumber: number = 240000
+  berforGuestsNumber: number = 32
+  toIncomeNumber: number = 520000
+  toGuestsNumber: number = 67
+  berforBrokerageFee: number = this.berforIncomeNumber / 10
+  toBrokerageFee: number = this.toIncomeNumber / 10
+
+  incomeChartOption: Chart.ChartOptions = {
+    scales: {
+      xAxes: [
+        {
+          scaleLabel: {
+            display: true
+          },
+          ticks: {
+            beginAtZero: true,
+            fontSize: 18
+          }
+        }
+      ],
+      yAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: '収益',
+            fontSize: 24
+          },
+          ticks: {
+            beginAtZero: true,
+            min: 0,
+            stepSize: 100000,
+            fontSize: 18
+          }
+        }
+      ]
+    }
+  }
+
+  guestsNumberChartOption: Chart.ChartOptions = {
+    scales: {
+      xAxes: [
+        {
+          scaleLabel: {
+            display: true
+          },
+          ticks: {
+            beginAtZero: true,
+            fontSize: 18
+          }
+        }
+      ],
+      yAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: '宿泊人数',
+            fontSize: 24
+          },
+          ticks: {
+            beginAtZero: true,
+            min: 0,
+            stepSize: 10,
+            fontSize: 18
+          }
+        }
+      ]
+    }
+  }
 }
 </script>
+
 <style lang="scss">
 .v-card {
   width: 100%;
